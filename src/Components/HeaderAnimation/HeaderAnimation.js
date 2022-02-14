bbf5e8dc16f82761db1486d46e7217fafbe7aa63
Francine <franmelopepe2@gmail.com>
2022-02-14 11:15:51 +0100

    navbar created

---
+++ src/Components/HeaderAnimation/HeaderAnimation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './HeaderAnimation.css';
+
+function HeaderAnimation() {
+return (
+    <div class="animated-title">
+        <div class="text-top">
+            <div>
+                <span>Hi. My name is Francine</span>
+                {/* <span></span> */}
+                {/* <span class="dot1"></span>
+                <span class="dot2"></span>
+                <span class="dot3"></span>
+                <span class="dot4"></span>
+                <span class="dot5"></span>
+                <span class="dot6"></span>
+                <span class="dot7"></span> */}
+
+            </div>
+        </div>
+        <div class="text-bottom">
+            <div>I am a Frontend Developer</div>
+        </div>
+    </div>
+    );
+
+}
+
+export default HeaderAnimation;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+    return(
+        <>
+        <div className="navbar_header">
+
+
+        </div>
+        
+        
+        
+        </>
+    );
+}
