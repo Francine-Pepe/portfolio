d3b976411f1bea04a706a7c131c898f2aae4f728
Francine <franmelopepe2@gmail.com>
2022-02-15 11:28:57 +0100

    main page design

--- src/Components/HeaderAnimation/HeaderAnimation.js
+++ src/Components/HeaderAnimation/HeaderAnimation.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './HeaderAnimation.css';
+import Arrow from './../Images/arrow_orange.png';
 
 function HeaderAnimation() {
 return (
+    <>
     <div class="animated-title">
         <div class="text-top">
             <div>
@@ -18,10 +20,26 @@ return (
 
             </div>
         </div>
+        
         <div class="text-bottom">
             <div>I am a Frontend Developer</div>
+            
+        </div>
+        
+    </div>
+    <div className="self_description">
+            <p>Creative, passionate about visual design and animation, <br /> with which I express myself in the most
+                <br /> effective and clean way</p>
+    </div>
+    <div className="photo_image">
+        <div className="my_work">
+            <p>this is <a href="/" >my work</a></p>
+            <a href="/"><img src={Arrow} alt="arrow" /></a>
+
         </div>
     </div>
+   
+    </>
     );
 
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './NavBar.css';
+import { Icon } from '@iconify/react';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
     return(
         <>
-        <div className="navbar_header">
-
-
+        <div className="navigation_container">
+            <div className="navbar_header">
+                <div className="header_navigation">
+                    <ul>
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/">Projects</NavLink></li>
+                        <li><NavLink to="/">Skills</NavLink></li>
+                        <li><NavLink to="/">Contact</NavLink></li>
+                    </ul>
+                </div>
+                <div className="social_media">
+                    <div className="social_icons">
+                        <Icon className="social_icons_icons" icon="akar-icons:github-fill" color="#153b61" width="30" height="30" />
+                        <Icon className="social_icons_icons" icon="entypo-social:linkedin-with-circle" color="#153b61" width="30" height="30" /> 
+                        <Icon className="social_icons_icons" icon="ant-design:behance-circle-filled" color="#153b61" width="30" height="30" /> 
+                        <Icon className="social_icons_icons" icon="entypo-social:instagram-with-circle" color="#153b61" width="30" height="30" /> 
+                    </div>                 
+                </div>
+                
+            </div>
         </div>
         
         
