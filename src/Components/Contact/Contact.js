056e314bdd91f6c467522137ef480bbdda6bd0eb
Francine <franmelopepe2@gmail.com>
2022-02-21 10:11:42 +0100

    contact form created

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,7 +3,7 @@ import emailjs from '@emailjs/browser';
 import { Container } from '@mui/material';
 import './Contact.css';
 import { Icon } from '@iconify/react';
-
+import Postit from './../Images/postit.png';
 
 export default function Contact () {
   const form = useRef();
@@ -11,16 +11,24 @@ export default function Contact () {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_5gpnfnk', 'template_sgmp2xj', e.target, 'user_YeUwC6WQsK1xiMGVBrKzC')
+    emailjs.sendForm('service_5gpnfnk', 'template_sgmp2xj', form.current, 'user_YeUwC6WQsK1xiMGVBrKzC')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
-      e.target.reset();
+      form.current.reset();
           alert('Thanks for contacting me! I will reply you soon! 📧') 
           
   };
+  
+//   const onSubmit = (data, r) => {
+//     alert(`Thank you for your message from ${data.email}`);
+//     const templateId = 'template_l7s9qxd';
+//     const serviceID = 'my_gmail';
+//     sendFeedback(serviceID, templateId, { from_name: data.name, message_html: data.comment, reply_to: data.email })
+//     r.target.reset();
+// }
 
   return (
     <>
@@ -30,7 +38,20 @@ export default function Contact () {
           <h3>Contact</h3>
         <hr />
       </header>
-      <Container fluid className="contact_container mt-5">                
+      <Container fluid className="contact_container mt-5"
+        sx={{
+          display: 'flex',
+          marginTop: 5,
+          
+        }}
+      
+      >   
+        <div className='postit_text'>
+          <p>Did you enjoy my work <br /> and would <br />like to develop a project?<br />Just drop me <br /> a line! <br /> Francine 🙂</p>
+          
+        </div>
+        <img className="postit" src={Postit} alt="postit" width="500" height="500" />
+        
         <form  className="form_box" ref={form} onSubmit={sendEmail}>
           <label>Name</label>
           <input type="text" name="user_name" />
@@ -38,7 +59,7 @@ export default function Contact () {
           <input type="email" name="user_email" />
           <label>Message</label>
           <textarea name="message" />
-          <input type="submit" value="Send" />
+          <button type="submit" value="Send" ><Icon icon="fluent:send-28-regular" color="#F8F8F8" width="40" height="35" /></button>
         </form>
       </Container>
     </>
